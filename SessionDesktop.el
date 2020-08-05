2a611a5f602eb9ddc8bac688de85cd4b9b32cc34
secwang <monadic.io@gmail.com>
2020-08-05 10:32:48 +0800

    add

--- SessionDesktop.el
+++ SessionDesktop.el
@@ -2,57 +2,32 @@
 ;; --------------------------------------------------------------------------
 ;; Desktop File for Emacs
 ;; --------------------------------------------------------------------------
-;; Created Tue Jan 28 10:27:25 2020
+;; Created Mon Jul 27 21:43:40 2020
 ;; Desktop file format version 208
 ;; Emacs version 25.3.50.1
 (setq revive:frame-configuration-to-restore
-  '(progn 
-     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 154) (left . 1349) (unsplittable) (modeline . t) (width . 72) (height . 48) (fringe) (mouse-color . "black") (environment) (visibility . t) (cursor-color . "Red") (background-mode . light) (fullscreen) (alpha) (scroll-bar-height . 0) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 0) (menu-bar-lines . 1) (right-fringe . 8) (left-fringe . 1) (line-spacing) (background-color . "White") (foreground-color . "Black") (horizontal-scroll-bars) (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(72 48 ((0 0 75 47)) (("~/workspace/org/home.org" "home.org" 472 143 ((tab "home.org" "/Users/secwang/workspace/org/home.org"))))))))
+  '(progn ))
 
 ;; Global section:
-(setq desktop-saved-frameset [frameset 1 (24111 39949 465100 0) (desktop . "208") "secwang@secwang.local" nil nil ((((font-backend mac-ct) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (horizontal-scroll-bars) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 1) (right-fringe . 8) (menu-bar-lines . 1) (tool-bar-lines . 0) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (scroll-bar-height . 0) (alpha) (fullscreen) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility . t) (environment) (mouse-color . "black") (fringe) (frameset--id . "1C0A-EF2E-49C8-9018") (frameset--mini t . t) (height . 48) (width . 72) (modeline . t) (minibuffer . t) (unsplittable) (left . 1349) (top . 154) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "secwang.local") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 2) (min-width-ignore . 9) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 32) (min-pixel-width-ignore . 59) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 528) (pixel-height . 752) (total-width . 75) (total-height . 47) (normal-height . 1.0) (normal-width . 1.0) (buffer "home.org" (selected . t) (hscroll . 0) (fringes 1 8 nil) (margins 3) (scroll-bars nil 3 t nil 0 nil) (vscroll . 0) (dedicated) (point . 472) (start . 143))))])
+(setq desktop-saved-frameset [frameset 1 (24350 55820 801446 0) (desktop . "208") "secwang@secwang.lan" nil nil ((((font-backend mac-ct) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (horizontal-scroll-bars) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 1) (right-fringe . 8) (menu-bar-lines . 1) (tool-bar-lines . 0) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (scroll-bar-height . 0) (alpha) (fullscreen) (fringe) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility) (frameset--id . "2F89-8C42-8308-9D57") (frameset--mini t . t) (height . 39) (width . 71) (modeline . t) (minibuffer . t) (unsplittable) (left . 1609) (top . 428) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "secwang.lan") (explicit-name . t) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 2) (min-width-ignore . 6) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 64) (min-pixel-width . 70) (min-pixel-height-ignore . 30) (min-pixel-width-ignore . 38) (min-pixel-height-safe . 16) (min-pixel-width-safe . 14)) leaf (pixel-width . 521) (pixel-height . 608) (total-width . 74) (total-height . 38) (normal-height . 1.0) (normal-width . 1.0) (buffer " *empty*" (selected . t) (hscroll . 0) (fringes 1 8 nil) (margins nil) (scroll-bars nil 3 t nil 0 nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))))])
 (setq desktop-missing-file-warning nil)
 (setq tags-file-name nil)
 (setq tags-table-list nil)
 (setq search-ring nil)
 (setq regexp-search-ring nil)
 (setq register-alist nil)
-(setq file-name-history '("~/workspace/org/home.org" "~/workspace/org/opportunity.org" "~/workspace/org/home.org" "~/Library/Application Support/Aquamacs Emacs/scratch buffer" "~/Desktop/timelog.org" "~/Desktop/today.org" "~/Desktop/talk.org" "~/Desktop/today.org" "~/Desktop/today" "/Volumes/[C] Windows 10.hidden/drakon_editor1.31/drakon_editor.tcl"))
+(setq file-name-history '("~/workspace/org/home.org" "~/workspace/org/writing.org" "~/workspace/org/tools.org" "~/workspace/org/writing.org" "~/workspace/org/home.org" "~/workspace/org/tmp.org" "~/workspace/org/opportunity.org" "~/workspace/org/tmp.org" "~/workspace/org/home.org" "~/workspace/org/opportunity.org" "~/workspace/org/home.org" "~/Library/Application Support/Aquamacs Emacs/scratch buffer" "~/Desktop/timelog.org" "~/Desktop/today.org" "~/Desktop/talk.org" "~/Desktop/today.org" "~/Desktop/today" "/Volumes/[C] Windows 10.hidden/drakon_editor1.31/drakon_editor.tcl"))
 
 ;; Buffer section -- buffers listed in same order as in buffer list:
-(desktop-create-buffer 208
-  "/Users/secwang/workspace/org/home.org"
-  "home.org"
-  'org-mode
-  '(auto-fill-mode smart-spacing-mode linum-mode)
-  472
-  '(551 nil)
-  nil
-  nil
-  '((buffer-file-coding-system . undecided-unix) (truncate-lines))
-  '((mark-ring (478))))
-
-(desktop-create-buffer 208
-  "/Users/secwang/workspace/org/opportunity.org"
-  "opportunity.org"
-  'org-mode
-  '(auto-fill-mode smart-spacing-mode linum-mode)
-  778
-  '(778 nil)
-  nil
-  nil
-  '((buffer-file-coding-system . utf-8-unix) (truncate-lines))
-  '((mark-ring nil)))
-
 (desktop-create-buffer 208
   "/Users/secwang/Library/Application Support/Aquamacs Emacs/scratch buffer"
   "*scratch*"
   'text-mode
-  '(visual-line-mode smart-spacing-mode linum-mode)
-  1
-  '(nil nil)
+  '(auto-fill-mode smart-spacing-mode linum-mode)
+  16
+  '(16 nil)
   nil
   nil
-  '((truncate-lines))
+  '((buffer-file-coding-system . utf-8-unix) (truncate-lines))
   '((mark-ring nil)))
 
